e68b5261fd77375cc002059c7a6aee2582a018d8
BuildTools <luminai@gmail.com>
2019-10-15 17:48:46 -0400

    removed conftest.py

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-@pytest.fixture(scope="session")
-def my_setup(request):
-    print('\nDoing setup')
-    def fin():
-        print ("\nDoing teardown")
-        request.addfinalizer(fin)
